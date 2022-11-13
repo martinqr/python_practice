8168c18c1f7a44b1881c08f4fb81b2e6c3042059
Martin Zalazar <martin.zqr@gmail.com>
2022-11-13 17:01:18 -0300

    operators.py

---
+++ day_2/variables.py
@@ -0,0 +1,88 @@
+# 'Day 2: 30 Days of python programming
+
+#level 1
+first_name = 'john'
+last_name = 'doe'
+full_name = 'john doe'
+country = 'neverland'
+city = 'fantasy'
+age = 30
+year = 2022
+is_married = True
+is_true = True
+is_light_on = False
+book,author,year = 'Harry Potter and the Philosophers Stone', 'J. K. Rowling' , 1997
+
+# -------------------------------------------------------------------------------- #
+
+#level 2
+
+# --------- 1 ---------
+
+# print(type(first_name))
+# print(type(last_name))
+# print(type(full_name))
+# print(type(country))
+# print(type(city))
+# print(type(age))
+# print(type(year))
+# print(type(is_married))
+# print(type(is_true))
+# print(type(is_light_on))
+# print(type(book))
+# print(type(author))
+# print(type(year))
+
+# --------- 2 ---------
+
+len(first_name)
+
+# --------- 3 ---------
+
+max(len(first_name),len(last_name))
+
+# --------- 4 ---------
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+
+diff = num_one - num_two
+
+product = num_one * num_two
+
+division = num_one / num_two
+
+remainder = num_two % num_one
+
+exp = num_one ** 2
+
+floor_division = num_one // num_two
+
+# --------- 5 ---------
+
+circle = 30
+
+# area_of_circle = 3.14 * ((circle / 2)**2)
+
+# circum_of_circle = 2 * 3.14 * (circle / 2)
+
+# radius = int( input( 'My radius is: ') )
+# area = int(3.14  *(radius ** 2))
+
+# first_name_rename = input('My first name is: ')
+# last_name_rename = input('My last name is: ')
+# country_rename = input('My country is: ')
+# age_rename = int( input('My age is: ' ))
+
+# first_name = first_name_rename
+# last_name = last_name_rename
+# country = country_rename
+# age = age_rename
+
+# print(first_name,last_name,country,age)
+
+help('keywords')
+
+
---
+++ day_3/operators.py
@@ -0,0 +1,170 @@
+import math
+
+###            1, 2, 3, 4            ###   
+age = 30
+height = 1.80
+complex_number = 1j
+
+# base_triangle = float(input('Base is: '))
+# height_triangle = float(input('Height is: '))
+# area = 0.5 * base_triangle * height_triangle
+
+# print('The area of the triangle is', area)
+
+###            5           ###  
+# side_a = float(input('side a is: '))
+# side_b = float(input('side b is: '))
+# side_c = float(input('side c is: '))
+# perimeter = side_a + side_b + side_c
+
+# print('The perimeter of the triangle is', perimeter)
+
+###            6            ###
+
+# length_rectangle = float(input('The length of the rectangle is: '))
+# width_rectangle = float(input('The width of the rectangle is: '))  
+
+# area_rectangle = length_rectangle * width_rectangle
+# perimeter_rectangle = 2 * (length_rectangle + width_rectangle)
+
+# print('The area of the rectangle is: ', area_rectangle)
+# print('The perimeter of the rectangle is: ', perimeter_rectangle)
+
+###            7            ###
+
+# radius_circle = float(input('The radius of the circle is: '))
+
+# area_circle = 3.14 * radius_circle * radius_circle
+# circumference_circle = 2 * 3.14 * radius_circle
+
+# print('The area of the circle is: ', area_circle )
+# print('The circumference of the circle is: ', circumference_circle)
+
+###            8            ###
+
+value_y1 = 0
+value_x2 = 0
+
+y = int(2*0-2)
+x = int((0 + 2) / 2)
+
+# print('x-intercept in: ',x, value_y1)
+# print('y-intercept in: ', value_x2, y)
+
+slope_ejercice_8 = float((value_y1 - y) / ( x - value_x2))
+
+# print('The slope is: ', slope)
+
+###            9            ###
+
+# Slope is (m = y2-y1/x2-x1). Find the slope and Euclidean 
+# distance between point (2, 2) and point (6,10)
+
+# x1 = 2
+# y1 = 2
+# x2 = 6
+# y2 = 10
+
+# slope_ejercice_9 = float((y2 - y1) / ( x2 - x1))
+# # eucledian = math.sqrt(((x1 - x2) **2) + ((y2 - y1) **2))
+
+
+# # # print('The slope is: ', slope)
+# # print('The eucledian distance is: ', eucledian)
+
+###            10            ###
+
+# print(slope_ejercice_9 > slope_ejercice_8)
+
+###            11            ###
+
+# x1 = (6 * -1 + (math.sqrt((6**2) - (4 * 1 * 9)))) / 2 * 1
+# x2 = (6 * -1 - (math.sqrt((6**2) - (4 * 1 * 9)))) / 2 * 1
+
+# # y = x ** 2 + 6 * x + 9
+
+# print('Values are: ',x1,x2)
+
+###            12           ###
+
+# print(len('python') > len('dragon'))
+
+# ###            13           ###
+
+# print( 'on' in 'python' and 'on' in 'dragon')
+
+###            14           ###
+
+# print( 'jargon' in 'I hope this course is not full of jargon')
+
+###            15           ###
+
+# print( 'on' not in 'python' and 'on' not in 'dragon')
+
+###            16          ###
+
+# txt = len('python')
+
+# txt_float = float(txt)
+
+# txt_str = str(txt_float)
+
+# print(txt_str)
+
+###            17          ###
+
+# num = float(input('Enter any number: '))
+
+
+# if( num % 2 == 0):
+#     print('The number is even')
+
+# else: 
+#     print('The number is odd')    
+
+
+###            18          ###
+
+# print( 7 // 3 == int(2.7))
+
+###            19          ###
+
+# print( type(10) == type('10'))
+
+###            20          ###
+
+# print(int('9.8') == 10)  --> invalid literal for int()
+
+###            21          ###
+
+# hours = int(input('hours: '))
+# rate_ph = int(input('rate per hour: '))
+# weekly_earning = hours * rate_ph
+
+# print('Your weekly earning is: ', weekly_earning)
+
+###            22          ###
+
+# years = int(input('number of years you have lived: ' ))
+# result = int(years * 31536000) ## sec in one year
+
+# print ('You have lived for:' , result ,'seconds')
+
+###            23          ###
+
+# a = 1
+# b = 2
+# c = 3
+# d = 4
+# e = 5
+
+# print(a, a, a, a, a)
+# print(b*a, a, b, b*b, b*d)
+# print(c*a, a, c, c*c, c*c*c)
+# print(d*a, a, d, d*d, d*d*d)
+# print(e*a, a, e, e*e, e*e*e)
+
+
+
+
+
