4ac470ede85523c82faae3f91bd20d2b745acd1c
Martin Zalazar <martin.zqr@gmail.com>
2022-11-15 18:47:29 -0300

    strings.py

---
+++ day_4/strings.py
@@ -0,0 +1,187 @@
+# #1
+
+# days_of_pythons = 'Thirty '+'Days '+'Of '+'Python'
+
+# #2
+
+coding_for_all = 'Coding for all'
+
+# #3
+
+company = coding_for_all
+
+# #4
+
+# print(company)
+
+# #5
+
+# print(len(company))
+
+# #6
+
+# company_upper = company.upper()
+
+# #7
+
+# company_lower = company.lower()
+
+# #8
+
+# company_capitalize = company.capitalize() #convert the first letter to capital letter
+# company_title = company.title() #return a title cased string
+# company_swapcase = company.swapcase() #covert all uppercase to lowecase
+
+# print(company_capitalize,company_title,company_swapcase)
+
+#9
+
+# company_slice = company[6:] #slice the first word of "coding for all"
+
+# print(company_slice)
+
+#10
+
+# print(company.index('Coding')) #Returns the lowest index of a substring
+
+#11
+
+# print(company.replace('Coding','Python')) #Replace a word or order (Old value,new value)
+
+#12
+
+# print(company.replace('Coding for all','Python for everyone'))
+
+#13
+
+# print(company.split( )) #return a list with using a separator , in this case is blank space.
+
+#14
+
+# social = 'Facebook,Google,Microsoft,Apple,IBM,Oracle,Amazon'
+# print(social.split(','))
+
+#15
+
+cfa = 'Coding For All'
+# print(str[0]) #Return first character of 'str'
+
+# #16
+
+# print(str[-1]) #Return last character of 'str
+
+# #17
+
+# print(str[10])
+
+#18
+
+# str_2 = 'Python For Everyone'
+# print(str_2.replace('Python For Everyone','PFE'))
+
+# #19
+
+# str_3 = 'Coding For All'
+# print(str_3.replace('Coding For All','CFA'))
+
+#20
+
+# str_4 = 'Coding For All'
+# # print(str_4.index('C'))
+
+# #21
+
+# print(str_4.index('F'))
+
+#22
+
+# print(cfa.rfind('l'))
+
+#23
+
+str_5 = 'You cannot end a sentence with because because because is a conjunction'
+
+# print(str_5.index('b'))
+
+#24
+
+# print(str_5.rindex('b'))
+
+#25
+
+str_6 = str_5[0:31]
+str_7 = str_5[55::]
+
+# print(str_6 + str_7)
+
+#26 and 27 are repeated
+
+#28
+
+# print(cfa.startswith('Coding')) #Return true because the string cfa starts with 'Coding'
+
+#29
+
+# print(cfa.startswith('coding')) #Return false because the string cfa doesnt starts with 'coding'
+
+#30
+
+# cfa_spaces = '   Coding For All      '
+
+# print(cfa_spaces.strip())
+
+#31
+
+# print('30DaysOfPython'.isidentifier() , 'thirty_days_of_python'.isidentifier()) 
+# # '30DaysOfPython' returns False and 'thirty_days_of_python' returns True
+
+#32
+
+# python_libraries = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+
+# print(' '.join(python_libraries)) #Join a list with a space within them
+
+
+#33
+
+# scape_setences = 'I am enjoying this challenge.\nI just wonder what is next. '
+
+# print(scape_setences)
+
+#34
+
+# scape_setences_2 = 'Name \tAge \tCountry \tCity\nAsabeneh 250 \tFinland \tHelsinki '
+
+# print(scape_setences_2)
+
+#35
+
+# radius = 10
+# area = 3.14 * radius ** 2
+
+# area_example_1 = f'The area of a circle with radius {radius} is {area} meters square'
+# area_example_2 = 'The area of a circle with radius {} is {} meters square'.format(radius,area)
+
+# print(area_example_1)
+# print(area_example_2)
+
+#36
+
+# a = 8
+# b = 6
+
+# print(f'{a} + {b} = {a+b}')
+# print(f'{a} - {b} = {a-b}')
+# print(f'{a} * {b} = {a*b}')
+# print(f'{a} / {b} = {a/b}')
+# print(f'{a} % {b} = {a%b}')
+# print(f'{a} // {b} = {a//b}')
+# print(f'{a} ** {b} = {a**b}')
+
+
+
+
+
+
+
+
