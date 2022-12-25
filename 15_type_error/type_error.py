aaf6ef74af0b818663f3e9672b821869c480c76e
Martin Zalazar <martin.zqr@gmail.com>
2022-12-25 14:48:03 -0300

    type_error.py

---
+++ 15_type_error/type_error.py
@@ -0,0 +1,32 @@
+# print 'hola'
+# SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+
+# print(name)
+# NameError: name 'name' is not defined
+
+# numbers = [1,2,3,4]
+# print(numbers[10])
+# IndexError: list index out of range
+
+# import testing
+# ModuleNotFoundError: No module named 'testing'
+
+# import math
+# math.recall
+# AttributeError: module 'math' has no attribute 'recall'
+
+# names = {'name':'Martin'}
+# names['lastname']
+# KeyError: 'lastname'
+
+# 4 + '2'
+# TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+# from math import name
+# ImportError: cannot import name 'name' from 'math' (unknown location)
+
+# int('12s2')
+# ValueError: invalid literal for int() with base 10: '12s2'  
+
+# 10 / 0
+# ZeroDivisionError: division by zero
